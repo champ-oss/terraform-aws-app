a74ec4d217095dd949d0a73a51afdf4d9d456054
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-07-07 15:13:33 -0500

    adding dns path

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -67,7 +67,7 @@ module "acm" {
 }
 
 module "core" {
-  source                    = "github.com/champ-oss/terraform-aws-core.git?ref=v1.0.113-55c4641"
+  source                    = "github.com/champ-oss/terraform-aws-core.git?ref=v1.0.114-758b2d1"
   git                       = local.git
   name                      = local.git
   vpc_id                    = data.aws_vpcs.this.ids[0]
