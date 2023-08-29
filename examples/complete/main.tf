a0706e742cc4e9bb85bd9e3f3627c2c41b234878
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-08-29 11:10:58 -0500

    testing new count on r53 hc

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -144,6 +144,7 @@ module "this" {
   health_check_grace_period_seconds = 5
   deregistration_delay              = 5
   retention_in_days                 = 3
+  desired_count                     = 0
   enable_execute_command            = true
 
   environment = {
