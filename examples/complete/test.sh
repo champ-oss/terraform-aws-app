2238d0605c771b35aecb47cc5a783163f547762c
Justin Reeves <reeves122@gmail.com>
2023-09-06 12:18:56 -0500

    query the parameter value

--- examples/complete/test.sh
+++ examples/complete/test.sh
@@ -1,8 +1,8 @@
 set -e
 
-aws ssm get-parameter --with-decryption --name $SSM_KMS_TEST_1
-aws ssm get-parameter --with-decryption --name $SSM_KMS_TEST_2
-aws ssm get-parameter --with-decryption --name $SSM_SSM_TEST_1
+aws ssm get-parameter --with-decryption --name $SSM_KMS_TEST_1 --query 'Parameter.Value'
+aws ssm get-parameter --with-decryption --name $SSM_KMS_TEST_2 --query 'Parameter.Value'
+aws ssm get-parameter --with-decryption --name $SSM_SSM_TEST_1 --query 'Parameter.Value'
 
-curl https://$DNS_NAME/ | grep Hello world
+curl https://$DNS_NAME/ | grep "Hello world"
 
