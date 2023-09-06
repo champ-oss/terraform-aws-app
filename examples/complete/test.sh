802d5bb7e965e90ef74d5b5e69206650962b7f46
Justin Reeves <reeves122@gmail.com>
2023-09-06 12:28:22 -0500

    grep for expected ssm values

--- examples/complete/test.sh
+++ examples/complete/test.sh
@@ -1,8 +1,11 @@
 set -e
 
-aws ssm get-parameter --with-decryption --name $SSM_KMS_TEST_1 --query 'Parameter.Value'
-aws ssm get-parameter --with-decryption --name $SSM_KMS_TEST_2 --query 'Parameter.Value'
-aws ssm get-parameter --with-decryption --name $SSM_SSM_TEST_1 --query 'Parameter.Value'
+# Validate the values of the KMS SSM parameters "KMSTEST1" and "KMSTEST2"
+aws ssm get-parameter --with-decryption --name $SSM_KMS_TEST_1 --query 'Parameter.Value' | grep "kms secret 1"
+aws ssm get-parameter --with-decryption --name $SSM_KMS_TEST_2 --query 'Parameter.Value' | grep "kms secret 2"
 
-curl https://$DNS_NAME/ | grep "Hello world"
+# Validate the KMS SSM parameter "SSMTEST2" which overrides a secret of the same name
+aws ssm get-parameter --with-decryption --name $SSM_SSM_TEST_1 --query 'Parameter.Value' | grep "kms secret 2
+
+curl -s https://$DNS_NAME/ | grep "Hello world"
 
