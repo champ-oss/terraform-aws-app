ed177bfa4dc05d8742ad47eb2209684aecdf04b5
Justin Reeves <reeves122@gmail.com>
2023-09-07 12:26:27 -0500

    create local and update route53_health_check_resource_path output

--- route53_health_check.tf
+++ route53_health_check.tf
@@ -3,20 +3,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
+locals {
+  route53_health_check_resource_path = (
+    var.enable_public_healthcheck_rule ?
+    "${aws_lb_target_group.this.health_check[0].path}?${var.healthcheck_query_parameter}=${random_password.healthcheck[0].result}" :
+    aws_lb_target_group.this.health_check[0].path
+  )
+}
+
 module "route53_health_check" {
   count  = var.enable_route53_health_check && aws_appautoscaling_target.this.min_capacity != 0 ? 1 : 0
   source = "github.com/champ-oss/terraform-aws-route53-health-check.git?ref=v1.0.6-ee66457"
   providers = {
     aws = aws.virginia
   }
-  git  = var.git
-  type = var.health_check_type
-  port = var.health_check_port
-  tags = merge(local.tags, var.tags, local.name_tag)
-  fqdn = try(aws_route53_record.this[0].name, "fallback")
-  resource_path = (
-    var.enable_public_healthcheck_rule ?
-    "${aws_lb_target_group.this.health_check[0].path}?${var.healthcheck_query_parameter}=${random_password.healthcheck[0].result}" :
-    aws_lb_target_group.this.health_check[0].path
-  )
+  git           = var.git
+  type          = var.health_check_type
+  port          = var.health_check_port
+  tags          = merge(local.tags, var.tags, local.name_tag)
+  fqdn          = try(aws_route53_record.this[0].name, "fallback")
+  resource_path = local.route53_health_check_resource_path
 }
