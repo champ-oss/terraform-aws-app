d50cb038703b84f792f7cfb4431bf2118bd256b6
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-10-11 09:03:46 -0500

    update r53 health check mod version

--- route53_health_check.tf
+++ route53_health_check.tf
@@ -9,7 +9,6 @@ locals {
 module "route53_health_check" {
   count         = var.enable_route53_health_check && aws_appautoscaling_target.this.min_capacity != 0 ? 1 : 0
   source        = "github.com/champ-oss/terraform-aws-route53-health-check.git?ref=v1.0.8-e059e1a"
-  git           = var.git
   type          = var.health_check_type
   port          = var.health_check_port
   tags          = merge(local.tags, var.tags, local.name_tag)
