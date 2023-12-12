d7067a8eafcd3834785a4d28307497d8b27f04e6
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-12-12 11:03:52 -0600

    output dns endpoint

--- outputs.tf
+++ outputs.tf
@@ -23,3 +23,9 @@ output "route53_health_check_resource_path" {
   sensitive   = true
   value       = var.enable_route53_health_check ? local.route53_health_check_resource_path : null
 }
+
+output "dns_endpoint" {
+  description = "output dns endpoint"
+  sensitive   = true
+  value       = var.enable_route53 ? aws_route53_record.this[0].name : null
+}
