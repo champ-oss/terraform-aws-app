c1d1effa7538db0aa9a1a4b6d2cb801cd03a6312
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-11 10:25:02 -0500

    test with enabled set to false

--- route53_health_check.tf
+++ route53_health_check.tf
@@ -1,16 +1,9 @@
-locals {
-  route53_health_check_resource_path = (
-    var.enable_public_healthcheck_rule ?
-    "${aws_lb_target_group.this[0].health_check[0].path}?${var.healthcheck_query_parameter}=${random_password.healthcheck[0].result}" : ""
-  )
-}
-
 resource "aws_route53_health_check" "this" {
   count             = var.enabled && var.enable_route53_health_check ? 1 : 0
   fqdn              = try(aws_route53_record.this[0].name, "fallback")
   port              = var.health_check_port
   type              = var.health_check_type
-  resource_path     = try(local.route53_health_check_resource_path, "")
+  resource_path     = try("${aws_lb_target_group.this[0].health_check[0].path}?${var.healthcheck_query_parameter}=${random_password.healthcheck[0].result}", "")
   failure_threshold = 3
   request_interval  = 30
   regions           = ["us-east-1", "us-west-1", "us-west-2"]
