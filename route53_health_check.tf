2723f827926ef3ce75c84d9b59a1d96b1bb1b2bb
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-24 09:46:21 -0500

    update hc min capacity lookup

--- route53_health_check.tf
+++ route53_health_check.tf
@@ -4,10 +4,11 @@ locals {
     try("${aws_lb_target_group.this[0].health_check[0].path}?${var.healthcheck_query_parameter}=${random_password.healthcheck[0].result}", "") :
     try(aws_lb_target_group.this[0].health_check[0].path, "")
   )
+  autoscaling_min_capacity = try(aws_appautoscaling_target.this[0].min_capacity, 0)
 }
 
 resource "aws_route53_health_check" "this" {
-  count             = var.enabled && var.enable_route53_health_check && aws_appautoscaling_target.this[0].min_capacity != 0 ? 1 : 0
+  count             = var.enabled && var.enable_route53_health_check && local.autoscaling_min_capacity != 0 ? 1 : 0
   fqdn              = try(aws_route53_record.this[0].name, "fallback")
   port              = var.health_check_port
   type              = var.health_check_type
