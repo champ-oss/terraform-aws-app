1c72ca9d3704f44089db8c1cb69071f62c1a3ca9
Phillip Langley <philliplangley@champmbplangley.lan>
2025-02-11 09:12:29 -0600

    test cloudwatch metric alarm

---
+++ cloudwatch_metric_alarm.tf
@@ -0,0 +1,39 @@
+resource "aws_cloudwatch_metric_alarm" "cpu_avg_utilization" {
+  count = var.enabled && var.metric_alarms_enabled ? 1 : 0
+
+  alarm_name          = "${var.name}-cpu-avg-utilization"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 5
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 60
+  statistic           = "Average"
+  threshold           = var.cpu_avg_utilization_threshold # 80
+  alarm_description   = "This metric monitors CPU utilization"
+  alarm_actions       = [var.sns_topic_arn]
+  ok_actions          = [var.sns_topic_arn]
+  dimensions = {
+    ClusterName = var.cluster
+    ServiceName = var.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "memory_avg_utilization" {
+  count = var.enabled && var.metric_alarms_enabled ? 1 : 0
+
+  alarm_name          = "${var.name}-memory-avg-utilization"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 5
+  metric_name         = "MemoryUtilization"
+  namespace           = "AWS/ECS"
+  period              = 60
+  statistic           = "Average"
+  threshold           = var.memory_avg_utilization_threshold # 80
+  alarm_description   = "This metric monitors memory utilization"
+  alarm_actions       = [var.sns_topic_arn]
+  ok_actions          = [var.sns_topic_arn]
+  dimensions = {
+    ClusterName = var.cluster
+    ServiceName = var.name
+  }
+}
