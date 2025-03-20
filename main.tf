9f34a4cb7ca71eb98fd400fb69f0f3d0722d4763
Phillip Langley <philliplangley@champmbplangley.lan>
2025-03-20 16:28:44 -0500

    add missing event bus name

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ data "aws_region" "this" {
   count = var.enabled ? 1 : 0
 }
 
+data "aws_caller_identity" "this" {
+  count = var.enabled ? 1 : 0
+}
+
 locals {
   tags = {
     git       = var.git
