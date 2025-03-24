12d879ee1e85b257714fd173850c567bea365baa
Phillip Langley <philliplangley@champmbplangley.lan>
2025-03-24 16:55:17 -0500

    test updated module

--- variables.tf
+++ variables.tf
@@ -512,8 +512,3 @@ variable "enable_ecs_auto_update" {
   type        = bool
   default     = false
 }
-
-variable "source_event_bus_name" {
-  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_event_bus#name"
-  type        = string
-}
