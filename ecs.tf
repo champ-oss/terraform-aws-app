3a8ceb1b0de46b1d57a55a7197d0cd3c497ee05a
Phillip Langley <philliplangley@champmbplangley.lan>
2025-08-14 14:19:04 -0500

    make secrets optional on paused

--- ecs.tf
+++ ecs.tf
@@ -13,7 +13,12 @@ locals {
       image       = var.image
       essential   = true
       environment = [for key, value in merge(var.environment, local.kms_secrets_sha) : { name = key, value = value }]
-      secrets     = [for key, value in merge(var.secrets, local.kms_ssm) : { name = key, valueFrom = value }]
+      secrets = !var.paused
+        ? [for key, value in merge(var.secrets, local.kms_ssm) : {
+          name      = key
+          valueFrom = value
+        }]
+        : []
       command     = var.command
       portMappings = [
         {
