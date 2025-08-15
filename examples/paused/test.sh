2a3c63d4eaad0e52231005336e103c8ca33e8fd6
Phillip Langley <philliplangley@champmbplangley.lan>
2025-08-15 10:45:25 -0500

    updated test on log group

--- examples/paused/test.sh
+++ examples/paused/test.sh
@@ -2,4 +2,6 @@ set -e
 
 # test if core and app can be paused via greenfield
 # check if log groups are created on app ecs cluster
-aws logs describe-log-groups --log-group-name-prefix "${GIT}/"paused" | grep -q "${GIT}/paused"
+aws logs describe-log-groups \
+  --log-group-name-prefix "${GIT}/paused" \
+  | grep -q "${GIT}/paused"
