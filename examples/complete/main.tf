9f33b0c68ec78392828179363d3ae2177833039d
neena-champ <neena.deshpande@champtitles.com>
2026-02-13 14:18:03 -0600

    Changed read only root file system to false CLOUD-4019

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -176,7 +176,7 @@ module "this" {
   max_capacity                       = 10
   scale_in_cooldown                  = 30
   scale_out_cooldown                 = 30
-  read_only_root_file_system         = true
+  read_only_root_file_system         = false
 
   environment = {
     this  = "that"
