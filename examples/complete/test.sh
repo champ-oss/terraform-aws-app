b43c2a15398755246579d23815e553f5472719ee
neena-champ <neena.deshpande@champtitles.com>
2026-02-17 15:01:14 -0600

    another test CLOUD-4024

--- examples/complete/test.sh
+++ examples/complete/test.sh
@@ -1,16 +1,16 @@
 set -e
 if [ "$ENABLED" = "true" ]; then
   # Validate the values of the KMS SSM parameters "KMSTEST1" and "KMSTEST2"
-  aws ssm get-parameter --with-decryption --name $SSM_KMS_TEST_1 --query 'Parameter.Value' | grep "kms secret 1"
-  aws ssm get-parameter --with-decryption --name $SSM_KMS_TEST_2 --query 'Parameter.Value' | grep "kms secret 2"
+  #aws ssm get-parameter --with-decryption --name $SSM_KMS_TEST_1 --query 'Parameter.Value' | grep "kms secret 1"
+  #aws ssm get-parameter --with-decryption --name $SSM_KMS_TEST_2 --query 'Parameter.Value' | grep "kms secret 2"
 
   # Validate the KMS SSM parameter "SSMTEST2" which overrides a secret of the same name
-  aws ssm get-parameter --with-decryption --name $SSM_SSM_TEST_1 --query 'Parameter.Value' | grep "kms secret 2"
+  #aws ssm get-parameter --with-decryption --name $SSM_SSM_TEST_1 --query 'Parameter.Value' | grep "kms secret 2"
 
-  curl -s https://$DNS_NAME/ | grep "Hello world"
+  #curl -s https://$DNS_NAME/ | grep "Hello world"
 
   ### Add sleep
-  
+  echo "Module is enabled and readonlyfilesystem is set to true"
   sleep 900
    
 else
