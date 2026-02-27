0911aaec40bc8efd544af7e86561d2a4111a17aa
Phillip Langley <philliplangley@champmbplangley.lan>
2026-02-27 11:05:22 -0600

    update cicd changes

--- examples/complete/test.sh
+++ examples/complete/test.sh
@@ -7,7 +7,12 @@ if [ "$ENABLED" = "true" ]; then
   # Validate the KMS SSM parameter "SSMTEST2" which overrides a secret of the same name
   aws ssm get-parameter --with-decryption --name $SSM_SSM_TEST_1 --query 'Parameter.Value' | grep "kms secret 2"
 
-  curl -s https://$DNS_NAME/ | grep "Hello world"
+    # Simple retry for curl (5 attempts, 30 sec apart)
+    for i in {1..5}; do
+      curl -s https://"$DNS_NAME"/ | grep "Hello world" && break
+      echo "Curl attempt $i failed, retrying..."
+      sleep 30
+    done
    
 else
   echo "Module is disabled, no resources created for this test"
